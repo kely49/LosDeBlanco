1bb2de37ee2342697b90513091f6943f68d7009e
unknown <kelileon@hotmail.com>
2019-03-15 13:45:22 +0100

    Clase HttpHandler: creado metodo que recibe la informacion de una URL y la transforma a un String

---
+++ HttpHandler.java
@@ -0,0 +1,49 @@
+package com.example.hp.proyectoldb;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpHandler {
+
+    public HttpHandler()
+    {
+
+    }
+
+    public String makeServiceCall(String reqUrl){
+        String response = "";
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            //Leer la respuesta
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            response = convertStreamToString(in);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+    private String convertStreamToString(InputStream is){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}
