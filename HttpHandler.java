4b28947096d8f27d9ae24d887149a2dbca4bb7c7
unknown <kelileon@hotmail.com>
2019-03-28 10:48:03 +0100

    añadido metodo PUT y DELETE

--- HttpHandler.java
+++ HttpHandler.java
@@ -3,7 +3,6 @@ package com.example.hp.proyectoldb;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,6 +10,7 @@ import java.io.OutputStream;
 
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class HttpHandler {
@@ -29,6 +29,9 @@ public class HttpHandler {
             //Leer la respuesta
             InputStream in = new BufferedInputStream(conn.getInputStream());
             response = convertStreamToString(in);
+
+            in.close();
+            conn.disconnect();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -60,6 +63,58 @@ public class HttpHandler {
             os.close();
             conn.disconnect();
 
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void crearPUT(String reqUrl,String json){
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("PUT");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.connect();
+
+            //Mandamos los datos
+            OutputStream os = new BufferedOutputStream(conn.getOutputStream());
+            os.write(json.getBytes());
+
+            //Limpiamos
+            os.flush();
+
+            //Escribimos la respuesta
+            InputStream is = conn.getInputStream();
+
+            is.close();
+            os.close();
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void eliminarDato(String reqUrl) {
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("DELETE");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "utf8_spanish_ci");
+            conn.connect();
+
+            //Enviamos la peticion
+            InputStream is = conn.getInputStream();
+
+            is.close();
+            conn.disconnect();
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
